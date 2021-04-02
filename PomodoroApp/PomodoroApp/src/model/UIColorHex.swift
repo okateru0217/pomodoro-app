b3870a74d3cd491d3d92a4f188fb337a4f7e066f
okateru0217 <okateru.0217@gmail.com>
2021-04-02 10:41:31 +0900

    Add:タイマーのデザイン
    ライブラリの追加、autolayout対応済

---
+++ PomodoroApp/PomodoroApp/src/model/UIColorHex.swift
@@ -0,0 +1,19 @@
+//
+//  UIColorHex.swift
+//  PomodoroApp
+//
+//  Created by 岡澤輝明 on 2021/04/01.
+//
+
+import UIKit
+import Foundation
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        let v = Int("000000" + hex, radix: 16) ?? 0
+        let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
+        let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
+        let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
+        self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
+    }
+}
