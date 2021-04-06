fe4168dceb42c9790608dd3d5bd0227c08602b43
okateru0217 <okateru.0217@gmail.com>
2021-04-06 19:00:04 +0900

    Fix:タイマー処理とController処理とファイルで分割
    TimerModelにタイマー処理を追加

--- PomodoroApp/PomodoroApp/src/controller/ViewController.swift
+++ PomodoroApp/PomodoroApp/src/controller/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 import MBCircularProgressBar
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    public static let viewController = ViewController()
+    
     @IBOutlet weak var minutesLabel: UILabel!
     @IBOutlet weak var secondsLabel: UILabel!
     @IBOutlet weak var timerCircle: MBCircularProgressBarView!
@@ -17,13 +19,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var settingButton: UIButton!
     
     private var timer = Timer()
-    private var seconds: Int = 0
-    private var minutes: Int = 0
-    private var limit = 1
-    private var restLimit = 1
     private let pomodoroTimerColor = "FE6348"
     private let restTimerColor = "1BD1AE"
-    private var isPomodoroTime = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,15 +28,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    enum OperationTimerButtonStatus: String {
+        case start = "開始"
+        case restart = "再開"
+        case suspend = "一時停止"
+        case rest = "休憩する"
+    }
     @IBAction func operationTimer(_ sender: Any) {
-        switchTimerProcess()
+        switchOperationTimerButtonStatus()
         switchCancelButtonEnabled()
     }
     
     @IBAction func cancelTimer(_ sender: Any) {
-        isPomodoroTime = true
+        TimerModel.timerModel.isPomodoroTime = true
         timer.invalidate()
-        setUpTimer()
+        setUpTimerLabel()
         timerCircle.value = 0
         timerAppearance()
         switchTimer()
@@ -49,17 +52,18 @@ class ViewController: UIViewController {
 
 // タイマーの見た目に関する処理
 private extension ViewController {
-    // ポモドーロタイマーの画面の外観
     func timerAppearance() {
-        if isPomodoroTime {
+        // ポモドーロタイマーの画面の外観
+        if TimerModel.timerModel.isPomodoroTime {
             timerScreenAppearance(timerColor: pomodoroTimerColor)
-            timerMinutesDisplay(setLimit: limit)
+            timerMinutesDisplay(setLimit: TimerModel.timerModel.limit)
             timerCircleAppearance(timerColor: pomodoroTimerColor)
             timerStartButtonAppearance(setStartTitle: "開始", timerColor: pomodoroTimerColor)
             timerCancelButtonAppearance(setCancelTitle: "キャンセル", timerColor: pomodoroTimerColor)
+        // 休憩タイマーの外観
         } else {
             timerScreenAppearance(timerColor: restTimerColor)
-            timerMinutesDisplay(setLimit: restLimit)
+            timerMinutesDisplay(setLimit: TimerModel.timerModel.restLimit)
             timerCircleAppearance(timerColor: restTimerColor)
             timerStartButtonAppearance(setStartTitle: "休憩する", timerColor: restTimerColor)
             timerCancelButtonAppearance(setCancelTitle: "休憩終了", timerColor: restTimerColor)
@@ -86,114 +90,106 @@ private extension ViewController {
     }
     // キャンセルボタンの外観
     func timerCancelButtonAppearance(setCancelTitle: String, timerColor: String) {
-        cancelTimerButton.setTitle(setCancelTitle, for: .normal)
-        cancelTimerButton.setTitleColor(UIColor(hex: timerColor, alpha: 0.75), for: .normal)
-        cancelTimerButton.backgroundColor = UIColor(hex: "FFFFFF", alpha: 0.75)
-        cancelTimerButton.isEnabled = false
+        if TimerModel.timerModel.isPomodoroTime {
+            cancelTimerButton.setTitle(setCancelTitle, for: .normal)
+            cancelTimerButton.setTitleColor(UIColor(hex: timerColor, alpha: 0.75), for: .normal)
+            cancelTimerButton.backgroundColor = UIColor(hex: "FFFFFF", alpha: 0.75)
+            cancelTimerButton.isEnabled = false
+        } else {
+            cancelTimerButton.setTitle(setCancelTitle, for: .normal)
+            cancelTimerButton.setTitleColor(UIColor(hex: timerColor), for: .normal)
+            cancelTimerButton.backgroundColor = UIColor(hex: "FFFFFF")
+            cancelTimerButton.isEnabled = true
+        }
+    }
+}
+
+// ボタンの表示によって処理を変える
+extension ViewController {
+    // タイマー操作ボタンの表示によって処理を変える
+    func switchOperationTimerButtonStatus() {
+        let operationStatus = OperationTimerButtonStatus(rawValue: operationTimerButton.currentTitle!)
+        switch operationStatus {
+        case .start, .rest:
+            setUpTimerLabel()
+            timer.invalidate()
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+            operationTimerButton.setTitle("一時停止", for: .normal)
+        case .restart:
+            timer.invalidate()
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+            operationTimerButton.setTitle("一時停止", for: .normal)
+        case .suspend:
+            suspendSetUpTimer()
+            timer.invalidate()
+            operationTimerButton.setTitle("再開", for: .normal)
+        default: break
+        }
+    }
+    // タイマー操作ボタンの表示によってキャンセルボタンの見た目を変更する
+    func switchCancelButtonEnabled() {
+        let operationStatus = OperationTimerButtonStatus(rawValue: operationTimerButton.currentTitle!)
+        if TimerModel.timerModel.isPomodoroTime {
+            switch operationStatus {
+            case .start:
+                cancelTimerButton.isEnabled = false
+                cancelTimerButton.setTitleColor(UIColor(hex: pomodoroTimerColor, alpha: 0.75), for: .normal)
+                cancelTimerButton.backgroundColor = UIColor(hex: "FFFFFF", alpha: 0.75)
+            case .restart, .suspend:
+                cancelTimerButton.isEnabled = true
+                cancelTimerButton.setTitleColor(UIColor(hex: pomodoroTimerColor), for: .normal)
+                cancelTimerButton.backgroundColor = UIColor(hex: "FFFFFF")
+            default: break
+            }
+        } else {
+            cancelTimerButton.isEnabled = true
+            cancelTimerButton.setTitleColor(UIColor(hex: restTimerColor), for: .normal)
+            cancelTimerButton.backgroundColor = UIColor(hex: "FFFFFF")
+        }
     }
 }
 
 // タイマー表示関連
-private extension ViewController {
+extension ViewController {
     // タイマーに分数, 秒数をセットする
-    func setUpTimer() {
-        minutes = isPomodoroTime ? limit : restLimit
-        seconds = 0
-        minutesLabel.text = String(minutes)
-        secondsLabel.text = String("0\(seconds)")
+    func setUpTimerLabel() {
+        TimerModel.timerModel.setUpTimer()
+        minutesLabel.text = String(TimerModel.timerModel.minutes)
+        secondsLabel.text = String("0\(TimerModel.timerModel.seconds)")
     }
-    
     // 一時停止時のタイムを秒数ラベルへ反映
     func suspendSetUpTimer() {
-        let suspendMinutes = minutes
-        let suspendSeconds = seconds
+        let suspendMinutes = TimerModel.timerModel.minutes
+        let suspendSeconds = TimerModel.timerModel.seconds
         let isSuspendSecondsMoreTen = suspendSeconds < 10
         minutesLabel.text = String(suspendMinutes)
         secondsLabel.text = isSuspendSecondsMoreTen ? String("0\(suspendSeconds)") : String(suspendSeconds)
     }
-        
+    
     // タイマーの時間に応じて、プログレスバーの表示を変更する
     func progressTimerCircle() {
         let oneMinutesSeconds = 60
-        timerCircle.maxValue = CGFloat(limit * oneMinutesSeconds)
-        timerCircle.value = CGFloat(timerCircle.maxValue - CGFloat((minutes * oneMinutesSeconds) + seconds))
+        timerCircle.maxValue = CGFloat(TimerModel.timerModel.limit * oneMinutesSeconds)
+        timerCircle.value = CGFloat(timerCircle.maxValue - CGFloat((TimerModel.timerModel.minutes * oneMinutesSeconds) + TimerModel.timerModel.seconds))
     }
-}
-
-// タイマー処理関連
-private extension ViewController {
     // タイマーを動かす処理
     @objc func updateTimer() {
-        seconds -= 1
-        // 秒数が-1以下の時、59に戻す
-        if seconds <= -1 {
-            minutes -= 1
-            seconds = 59
-        }
-        // タイマー終了時の処理
-        if minutes == 0 && seconds == 0 {
-            isPomodoroTime = !isPomodoroTime
-            switchTimer()
+        TimerModel.timerModel.updateTimer()
+        if TimerModel.timerModel.minutes == 0 && TimerModel.timerModel.seconds == 0 {
             switchCancelButtonEnabled()
+            switchTimer()
         }
         // 秒数が10未満の表示を2桁の0埋めにする
-        let isSecondsMoreTen = seconds < 10
-        minutesLabel.text = String(minutes)
-        secondsLabel.text = isSecondsMoreTen ? String("0\(seconds)") : String(seconds)
+        let isSecondsMoreTen = TimerModel.timerModel.seconds < 10
+        minutesLabel.text = String(TimerModel.timerModel.minutes)
+        secondsLabel.text = isSecondsMoreTen ? String("0\(TimerModel.timerModel.seconds)") : String(TimerModel.timerModel.seconds)
         progressTimerCircle()
     }
-    
     // タイマーが0になった時に呼び出される処理
     func switchTimer() {
         timer.invalidate()
-        setUpTimer()
+        setUpTimerLabel()
         timerCircle.value = 0
         timerAppearance()
-    }
-    
-    // operationTimerButtonのテキストに応じて、タイマーの処理を変更する
-    func switchTimerProcess() {
-        if operationTimerButton.currentTitle == "開始" || operationTimerButton.currentTitle == "休憩する" {
-            setUpTimer()
-            timer.invalidate()
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
-            operationTimerButton.setTitle("一時停止", for: .normal)
-        } else if operationTimerButton.currentTitle == "一時停止" {
-            suspendSetUpTimer()
-            timer.invalidate()
-            operationTimerButton.setTitle("再開", for: .normal)
-        } else if operationTimerButton.currentTitle == "再開" {
-            timer.invalidate()
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
-            operationTimerButton.setTitle("一時停止", for: .normal)
-        }
-    }
-}
-
-// キャンセルボタン関連
-private extension ViewController {
-    // キャンセルボタンの押下可否判定
-    func switchCancelButtonEnabled() {
-        isPomodoroTime ? pomodoroCancelButtonAppearance() : restCancelButtonAppearance()
-    }
-    
-    // ポモドーロタイマーのキャンセルボタン押下可否判定
-    func pomodoroCancelButtonAppearance() {
-        if operationTimerButton.currentTitle == "開始" {
-            cancelTimerButton.isEnabled = false
-            cancelTimerButton.setTitleColor(UIColor(hex: pomodoroTimerColor, alpha: 0.75), for: .normal)
-            cancelTimerButton.backgroundColor = UIColor(hex: "FFFFFF", alpha: 0.75)
-        } else {
-            cancelTimerButton.isEnabled = true
-            cancelTimerButton.setTitleColor(UIColor(hex: pomodoroTimerColor), for: .normal)
-            cancelTimerButton.backgroundColor = UIColor(hex: "FFFFFF")
-        }
-    }
-    
-    // 休憩終了ボタンの外観
-    func restCancelButtonAppearance() {
-        cancelTimerButton.isEnabled = true
-        cancelTimerButton.setTitleColor(UIColor(hex: restTimerColor), for: .normal)
-        cancelTimerButton.backgroundColor = UIColor(hex: "FFFFFF")
     }
 }
---
+++ PomodoroApp/PomodoroApp/src/model/TimerModel.swift
@@ -0,0 +1,40 @@
+//
+//  TimerModel.swift
+//  PomodoroApp
+//
+//  Created by 岡澤輝明 on 2021/04/06.
+//
+
+import UIKit
+import Foundation
+
+final class TimerModel {
+    public static let timerModel = TimerModel()
+    private init() {}
+    
+    private (set) internal var minutes: Int = 0
+    private (set) internal var seconds: Int = 0
+    private (set) internal var limit = 1
+    private (set) internal var restLimit = 1
+    internal var isPomodoroTime = true
+    
+    // タイマーセット
+    func setUpTimer() {
+        minutes = isPomodoroTime ? limit : restLimit
+        seconds = 0
+    }
+    
+    // タイマーを動かす処理
+    func updateTimer() {
+        seconds -= 1
+        // 秒数が-1以下の時、59に戻す
+        if seconds <= -1 {
+            minutes -= 1
+            seconds = 59
+        }
+        // タイマー終了時の処理
+        if minutes == 0 && seconds == 0 {
+            isPomodoroTime = !isPomodoroTime
+        }
+    }
+}
