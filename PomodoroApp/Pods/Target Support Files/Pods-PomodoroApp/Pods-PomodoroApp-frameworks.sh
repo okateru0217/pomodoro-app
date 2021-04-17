7649ea2eef5069a885373f6629e541d1af579976
okateru0217 <okateru.0217@gmail.com>
2021-04-17 09:43:43 +0900

    Add:Chartsライブラリ

--- PomodoroApp/Pods/Target Support Files/Pods-PomodoroApp/Pods-PomodoroApp-frameworks.sh
+++ PomodoroApp/Pods/Target Support Files/Pods-PomodoroApp/Pods-PomodoroApp-frameworks.sh
@@ -175,10 +175,12 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Eureka/Eureka.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBCircularProgressBar/MBCircularProgressBar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Eureka/Eureka.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBCircularProgressBar/MBCircularProgressBar.framework"
 fi
